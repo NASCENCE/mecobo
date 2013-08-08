fa9056aaa8935318a618a4b351dcc479312d4286
Odd Rune <oddrunesl@gmail.com>
2013-08-08 16:18:22 +0200

    inital commit of sketch of fabric

---
+++ fpga/fabric.v
@@ -0,0 +1,11 @@
+module fabric (clk, res, in_pins, out_pins, addr, data, wr, cs)
+
+//Clock in configuration data for each pin-mux.
+//Config data is simple, the number in the register is
+//the physical output pin. 
+//always @ ... 
+
+//Generate the required muxes here.
+//genvar i ...
+
+endmodule
