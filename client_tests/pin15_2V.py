af1e0d3d946ff83edb8f6c2e152535faaa635081
atypic <lykkebo@idi.ntnu.no>
2014-08-28 09:51:41 +0100

    ah

--- client_tests/pin15_2V.py
+++ client_tests/pin15_2V.py
@@ -25,8 +25,8 @@ cli.clearSequences()
 it = emSequenceItem()
 it.pin = [15]
 it.startTime = 0
-it.endTime = 1200
-it.amplitude = 255
+it.endTime = 1000
+it.amplitude = 254
 it.operationType = emSequenceOperationType().CONSTANT   #implies analogue 
 cli.appendSequenceAction(it)
 
@@ -43,6 +43,7 @@ cli.joinSequences()
 
 res = []
 for i in cli.getRecording(0).Samples:
+  print i
   res.append(i * (5.0/4096.0));
 
 plt.ylim(-6,6)
