c490c802df324f34d0ffb15c076b7959abd9659e
simonharding <simon.harding@iname.com>
2014-09-19 15:33:03 +0100

    Added test routine
    run "EMLogServer" first to get a server, then on the SAME MACHINE, run
    "EMLogServer test"

--- EMServer/EMLogServer/Program.cs
+++ EMServer/EMLogServer/Program.cs
@@ -1,9 +1,11 @@
 ﻿using emInterfaces;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Thrift.Protocol;
 using Thrift.Server;
 using Thrift.Transport;
 
@@ -11,10 +13,8 @@ namespace EMLogServer
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void RunServer()
         {
-            Settings.LoadSettings(Settings.DefaultSettingsFileName);
-
             emLogServerImplementation handler = new emLogServerImplementation();
             emLogServer.Processor processor = new emLogServer.Processor(handler);
             TServerTransport serverTransport = new TServerSocket(Settings.GetSetting<int>("LogServerPort"));
@@ -22,6 +22,37 @@ namespace EMLogServer
 
             Reporting.Say("Starting log server on port " + Settings.GetSetting<int>("LogServerPort"));
             server.Serve();
+        }
+
+        public static void RunTest()
+        {
+            emLogServer.Client LogServerClient = null;
+            emLogServerSettings LogServerSettings = null;
+            TSocket transport = new TSocket("localhost", Settings.GetSetting<int>("LogServerPort"));
+                transport.Open();
+                TBinaryProtocol protocol = new TBinaryProtocol(transport);
+                LogServerClient = new emLogServer.Client(protocol);
+                
+
+            string LogServerExperimentName = LogServerClient.createUniqueExperimentName("Test");
+            LogServerSettings = LogServerClient.getLogServerSettings(LogServerExperimentName);
+            LogServerClient.log(LogServerSettings, "Hello world", emLogEventType.MISC);
+            LogServerClient.log(LogServerSettings, DateTime.Now.ToLongDateString(), emLogEventType.COMMAND);
+            LogServerClient.log(LogServerSettings, DateTime.Now.ToLongTimeString(), emLogEventType.COMMAND);
+        }
+
+        static void Main(string[] args)
+        {
+            Settings.LoadSettings(Settings.DefaultSettingsFileName);
+
+            foreach(string s in args)
+                if (s == "test")
+                {
+                    RunTest();
+                    return;
+                }
+
+            RunServer();
         }
     }
 }
